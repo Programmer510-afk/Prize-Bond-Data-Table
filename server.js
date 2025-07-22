7b3fcbf83cdd0cd20e14da7b605eabbb341a9080
Programmer 5.1 0 <abiduzzamanrahat5@gmail.com>
2025-07-22 12:21:11 +0600

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // Excel ফাইলের পাথ (আপনার GitHub repo তে আপলোড করা ফাইল ডাউনলোড করে রাখবেন Render সার্ভারে অথবা সরাসরি raw URL থেকে fetch করবেন)
-const EXCEL_URL = "https://raw.githubusercontent.com/Programmer510-afk/Prize-Bond-Data-Table/main/Prize%20Bond%20(Excel-Table).xlsx";
+const EXCEL_URL = "https://raw.githubusercontent.com/Programmer510-afk/Prize-Bond-Data-Table/main/Prize_Bond_Excel_Table.xlsx
+";
 
 // node-fetch লাগলে ইনস্টল করবেন: npm i node-fetch@2
 const fetch = require('node-fetch');
